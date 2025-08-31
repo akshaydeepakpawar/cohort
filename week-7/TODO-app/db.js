fa128ff226c90aaba4be6af2e5d005393a0ffc5c
akshay pawar <ap3690700@gmail.com>
2025-08-31 16:58:37 +0530

    improvements in auth

--- week-7/TODO-app/db.js
+++ week-7/TODO-app/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
 const User=new Schema({
-    email :String,
+    email :{type:String,unique:true},
     password:String,
     name:String
 })
@@ -17,6 +17,6 @@ const UserModel=mongoose.model("users",User);
 const TodoModel=mongoose.model("todos",Todo);
 
 module.exports={
-    UserModel:UserModel,
-    TodoModel:TodoModel
+    UserModel,
+    TodoModel
 }
--- week-7/TODO-app/index.js
+++ week-7/TODO-app/index.js
@@ -1,70 +1,107 @@
-const express=require("express");
-const jwt=require("jsonwebtoken");
-const SECRET_JWT="akshay"
-const {UserModel,TodoModel}=require("./db")
-const mongoose=require("mongoose");
-const app=express();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const SECRET_JWT = "akshay";
+const { UserModel, TodoModel } = require("./db");
+const mongoose = require("mongoose");
+const {z}=require("zod")
+const app = express();
 app.use(express.json());
-mongoose.connect("")
+mongoose.connect(
+  "mongodb+srv://akshay:yepasshe@cluster0.th96dyz.mongodb.net/todo-akshay-786"
+);
 
-app.post("/signup",async (req,res)=>{
-   const { email, password, name } = req.body;
-    await UserModel.create({
-        email,
-        password,
-        name
-    })
+app.post("/signup", async (req, res) => {
+  const requireBody=z.object({
+      email:z.email(),
+      name:z.string().min(10).max(100),
+      password:z.string()
+      .min(8, "Password must be at least 8 characters long").max(30,"maximum 30 characters only")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character")
+  })
+  const parsedDataWithSuccess=requireBody.safeParse(req.body);
+
+  if(!parsedDataWithSuccess.success)
+  {
     res.json({
-        message:"you are logged in"
+      message: "incorrect format",
+      error:parsedDataWithSuccess.error
     })
-})
-app.post("/signin",async(req,res)=>{
-    const { email, password } = req.body;
-    const user=await UserModel.findOne({
-        email,
-        password
-    })
+    return
+  }
 
-    if(user)
-    {
-        const token =jwt.sign({id:user._id},SECRET_JWT);
-        res.json({
-            token:token
-        })
-    }
-    else {
-        res.status(403).json({
-            message:"incorrect credentials"
-        })
-    }
-})
+  const { email, password, name } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+    await UserModel.create({
+      email: email,
+      password: hashedPassword,
+      name: name
+    });
+    res.json({
+      message: "you are logged in",
+    });
+  } catch (error) {
+    res.json({
+      message: "user already exists",
+    });
+  }
+});
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({
+    email: email
+  });
+  if (!user) {
+    res.status(403).json({
+      message: "invalid user please signup first",
+    });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign({ id: user._id }, SECRET_JWT);
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "incorrect credentials",
+    });
+  }
+});
 
 function authMiddleware(req, res, next) {
   // read the token from headers
-  const token = req.headers['token'];  
+  const token = req.headers["token"];
 
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
   }
   try {
-    const decoded=jwt.verify(token,SECRET_JWT);
-    req.userId=decoded.id;
+    const decoded = jwt.verify(token, SECRET_JWT);
+    req.userId = decoded.id;
     next();
   } catch (error) {
     return res.status(401).json({
-        error: "Invalid token" 
-    })
+      error: "Invalid token",
+    });
   }
 }
 
-app.post("/todo",authMiddleware,async(req,res)=>{
-    try {
+app.post("/todo", authMiddleware, async (req, res) => {
+  try {
     const { title, done } = req.body;
 
     await TodoModel.create({
       title,
       done,
-      userId: req.userId
+      userId: req.userId,
     });
 
     res.json({ message: "your todo is added" });
@@ -72,17 +109,17 @@ app.post("/todo",authMiddleware,async(req,res)=>{
     console.error(e);
     res.status(500).json({ message: "something went wrong" });
   }
-})
-app.get("/todos",authMiddleware,async(req,res)=>{
-    try {
-        const todos=await TodoModel.find({userId:req.userId})
-        res.json({todos})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "something went wrong" });
-    }
+});
+app.get("/todos", authMiddleware, async (req, res) => {
+  try {
+    const todos = await TodoModel.find({ userId: req.userId });
+    res.json({ todos });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("app listening on port 3000");
 });
